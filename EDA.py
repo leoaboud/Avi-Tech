b7d1789cc282488b484ad018cdf9d1a64b80b92c
leoaboud <leoaboud99@gmail.com>
2026-02-27 08:49:28 -0300

    RUL Calculation
    Implemented the calculation of Remaining Useful Life (RUL) for each engine unit based on its maximum observed cycle.
    
    Computed max cycle per unit
    
    Derived RUL as: max_cycle - current_cycle
    
    Applied clipping at 125 cycles (industry standard)
    
    This step transforms raw time-series data into a target variable for predictive maintenance modeling.

--- EDA.py
+++ EDA.py
@@ -1,33 +1,33 @@
-# =============================================
-# BAIXAR DADOS
-# =============================================
-
+# 1. IMPORTS
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from pathlib import Path
 import kagglehub
 
-# Configurações visuais
-plt.style.use('seaborn-v0_8-whitegrid')
-sns.set_palette("viridis")
-
-# Criar pastas
-Path("images").mkdir(exist_ok=True)
-Path("data").mkdir(exist_ok=True)
-
-# Download dataset
-print("Baixando dataset...")
+# 2. LOAD DATA
 path = kagglehub.dataset_download("bishals098/nasa-turbofan-engine-degradation-simulation")
 DATA_DIR = Path(path)
 
-# Encontrar arquivos
 train_path = next(DATA_DIR.rglob("train_FD001.txt"))
 test_path = next(DATA_DIR.rglob("test_FD001.txt"))
 rul_path = next(DATA_DIR.rglob("RUL_FD001.txt"))
 
+columns = ['unit','cycle'] + [f'sensor{i}' for i in range(1,22)]
+
+df = pd.read_csv(train_path, sep=r'\s+', header=None, names=columns)
+
+max_cycles = df.groupby('unit')['cycle'].max()
+
+df['RUL'] = df.apply(
+    lambda row: max_cycles[row['unit']] - row['cycle'],
+    axis=1
+)
+
 print("Caminho dataset:", DATA_DIR)
+print("Dados carregados")
 print("Train encontrado:", train_path.exists())
 print("Test encontrado:", test_path.exists())
 print("RUL encontrado:", rul_path.exists())
+print("Shape:", df.shape)
+print("Colunas:", df.columns.tolist())
+print(df.head())
+print("RUL criado")
